ef20b6ef0d50682d492aa773b4b3b7ab2bc0becd
Max Maass <mmaass@seemoo.tu-darmstadt.de>
2017-03-23 15:26:58 +0100

    Launch browser fullscreen

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 screen -d -m ./_background.sh
 sleep 1
-chromium-browser web-frontend/demo.html >/dev/null 2>/dev/null &
+chromium-browser --app=file:///home/pi/nfcgate-hnf-demo/web-frontend/demo.html --start-fullscreen >/dev/null 2>/dev/null &
